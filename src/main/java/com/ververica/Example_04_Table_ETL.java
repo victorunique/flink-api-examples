3833f6720849bfb1fc907a55ce2ab949af5fc874
Timo Walther <twalthr@apache.org>
2021-12-07 11:06:07 +0100

    Use Flink 1.14.0 instead of 1.15-SNAPSHOT

--- src/main/java/com/ververica/Example_04_Table_ETL.java
+++ src/main/java/com/ververica/Example_04_Table_ETL.java
@@ -1,13 +1,11 @@
 package com.ververica;
 
 import org.apache.flink.table.api.EnvironmentSettings;
-import org.apache.flink.table.api.JsonOnNull;
 import org.apache.flink.table.api.TableEnvironment;
-import org.apache.flink.table.expressions.TimePointUnit;
 
 import java.time.LocalDate;
 
-import static org.apache.flink.table.api.Expressions.*;
+import static org.apache.flink.table.api.Expressions.row;
 
 /** Use built-in functions to perform streaming ETL i.e. convert records into JSON. */
 public class Example_04_Table_ETL {
@@ -20,13 +18,14 @@ public class Example_04_Table_ETL {
             row(32L, "Bob", LocalDate.of(1990, 10, 14)),
             row(7L, "Kyle", LocalDate.of(1979, 2, 23)))
         .as("c_id", "c_name", "c_birthday")
-        .select(
-            jsonObject(
-                JsonOnNull.NULL,
-                "name",
-                $("c_name"),
-                "age",
-                timestampDiff(TimePointUnit.YEAR, $("c_birthday"), currentDate())))
+        // only available in Flink 1.15
+        //        .select(
+        //            jsonObject(
+        //                JsonOnNull.NULL,
+        //                "name",
+        //                $("c_name"),
+        //                "age",
+        //                timestampDiff(TimePointUnit.YEAR, $("c_birthday"), currentDate())))
         .execute()
         .print();
   }
